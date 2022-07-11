46f492061f2c6020ef85cbd50f1f66be9fa9068e
kiriakos <kiris3992@gmail.com>
2022-07-11 22:06:28 +0300

    Class Product

---
+++ Acropoli/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Product
+{
+    public int Id { get; set; } 
+    public string Name { get; set; }
+
+}
